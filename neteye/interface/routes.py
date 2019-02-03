dd876ac948a230ad059a1b64f72e4eb1049dd477
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-02-03 16:17:04 +0900

    add filter process in Interface

--- neteye/interface/routes.py
+++ neteye/interface/routes.py
@@ -16,3 +16,15 @@ def index():
     interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).paginate(page, 3)
     return render_template('interface/index.html', interfaces=interfaces)
 
+@interface_bp.route('/filter')
+def filter():
+    page = request.args.get('page', 1, type=int)
+    field = request.args.get('field')
+    filter_str = request.args.get('filter_str')
+    if field == 'ip_address':
+        interfaces = Interface.query.filter(Interface.ip_address.contains(filter_str)).paginate(page, 3)
+    elif field == 'description':
+        interfaces = Interface.query.filter(Interface.description.contains(filter_str)).paginate(page, 3)
+    elif field == 'node':
+        interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).filter(Node.hostname.contains(filter_str)).paginate(page, 3)
+    return render_template('interface/index.html', interfaces=interfaces)
