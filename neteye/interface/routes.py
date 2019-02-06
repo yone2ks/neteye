47f54106b383b527c60a49d7b542dd201b4f5e8c
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-02-07 00:24:27 +0900

    add PER_PAGE params and fix pagenation view in interface/index.html

--- neteye/interface/routes.py
+++ neteye/interface/routes.py
@@ -6,14 +6,14 @@ from neteye.node.models import Node
 from flask import request, redirect, url_for, render_template, flash, session
 import netmiko
 import pandas as pd
-
+from dynaconf import settings
 
 interface_bp = bp_factory('interface')
 
 @interface_bp.route('')
 def index():
     page = request.args.get('page', 1, type=int)
-    interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).paginate(page, 3)
+    interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).paginate(page, settings.PER_PAGE)
     return render_template('interface/index.html', interfaces=interfaces)
 
 @interface_bp.route('/filter')
@@ -22,9 +22,9 @@ def filter():
     field = request.args.get('field')
     filter_str = request.args.get('filter_str')
     if field == 'ip_address':
-        interfaces = Interface.query.filter(Interface.ip_address.contains(filter_str)).paginate(page, 3)
+        interfaces = Interface.query.filter(Interface.ip_address.contains(filter_str)).paginate(page, settings.PER_PAGE)
     elif field == 'description':
-        interfaces = Interface.query.filter(Interface.description.contains(filter_str)).paginate(page, 3)
+        interfaces = Interface.query.filter(Interface.description.contains(filter_str)).paginate(page, settings.PER_PAGE)
     elif field == 'node':
-        interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).filter(Node.hostname.contains(filter_str)).paginate(page, 3)
+        interfaces = Interface.query.join(Node, Interface.node_id==Node.id).add_columns(Interface.id, Node.hostname, Interface.name, Interface.ip_address).filter(Node.hostname.contains(filter_str)).paginate(page, settings.PER_PAGE)
     return render_template('interface/index.html', interfaces=interfaces)
