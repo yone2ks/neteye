b72d02fa4817807d842b5a7764d113a00eebbeaf
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-02-11 01:07:49 +0900

    fix overwrite file in flasknize.py

--- neteye/base/templates/flasknize.py
+++ neteye/base/templates/flasknize.py
@@ -4,4 +4,8 @@ from html_flasknize import HtmlFlasknize
 filename = sys.argv[1]
 hf = HtmlFlasknize(filename)
 hf.change_all()
-print(hf.output())
+with open(filename, "w") as f:
+    f.write(hf.output())
+
+
+
