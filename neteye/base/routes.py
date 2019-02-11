5b6dc56a02ad77d251a013616ea6bbfc7e5f5d8e
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-02-11 10:42:01 +0900

    fix routing for core-ui link in base/routes.py

--- neteye/base/routes.py
+++ neteye/base/routes.py
@@ -7,3 +7,13 @@ base_bp = bp_factory('base')
 @base_bp.route('/')
 def index():
     return render_template('index.html')
+
+@base_bp.route('/<path>')
+def template(path):
+    return render_template(path)
+
+@base_bp.route('/<directory>/<path>')
+def subdir_template(directory, path):
+    return render_template(directory + '/' + path)
+
+
