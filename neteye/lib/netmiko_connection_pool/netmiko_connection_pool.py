8e080bd6872d62c5502d1e5c706dcba56c1171fd
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-09-07 17:28:49 +0900

    add netmiko_connection_pool

---
+++ neteye/lib/netmiko_connection_pool/netmiko_connection_pool.py
@@ -0,0 +1,27 @@
+import netmiko
+from netmiko.ssh_autodetect import SSHDetect
+
+class ConnectionPool():
+    def __init__(self, pool_size=50):
+        self.pool = {}
+        self.pool_size = pool_size
+        self.keepalive_time = 10
+
+    def add_connection(self, params):
+        params['keepalive'] = self.keepalive_time
+        connection = netmiko.ConnectHandler(**params)
+        self.pool[params['ip']] = connection
+
+    def delete_connection(self, ip):
+        self.pool[ip].disconnect()
+        del self.pool[ip]
+
+    def get_connection(self, ip):
+        return self.pool[ip]
+
+    def _connection_exists(self):
+        return ip in self.pool
+
+    def size(self):
+        return len(self.pool)
+
---
+++ neteye/lib/netmiko_connection_pool/test.py
@@ -0,0 +1,20 @@
+import time
+from netmiko_connection_pool import ConnectionPool
+
+pool = ConnectionPool()
+ip = '192.168.94.100'
+params = {
+            'device_type': 'cisco_ios',
+            'ip': '192.168.94.100',
+            'username': 'admin',
+            'password': 'cisco',
+            'secret': 'cisco'
+        }
+pool.add_connection(params)
+print('show ip arp')
+print(pool.get_connection(ip).send_command('show ip arp'))
+print('')
+print('show inventory')
+print(pool.get_connection(ip).send_command('show inventory'))
+
+
