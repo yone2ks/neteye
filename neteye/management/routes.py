03f3d7e19818b785b844356e143a8411d078f765
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2019-09-22 21:08:15 +0900

    add troubleshoot and management

---
+++ neteye/management/routes.py
@@ -0,0 +1,23 @@
+from neteye.extensions import db, connection_pool
+from neteye.blueprints import bp_factory
+from flask import request, redirect, url_for, render_template, flash, session
+import netmiko
+import pandas as pd
+from dynaconf import settings
+
+management_bp = bp_factory('management')
+
+@management_bp.route('/connection_pool')
+def connection_pool_index():
+    return render_template('management/connection_pool_index.html', connection_pool=connection_pool.pool)
+
+@management_bp.route('/connection_pool/<ip>/reconnect')
+def connection_pool_reconnect(ip):
+    connection_pool.recreate_connection(ip)
+    return render_template('management/connection_pool_index.html', connection_pool=connection_pool.pool)
+
+@management_bp.route('/connection_pool/<ip>/delete', methods=['POST'])
+def delete(ip):
+    connection_pool.delete_connection(ip)
+    return render_template('management/connection_pool_index.html', connection_pool=connection_pool.pool)
+
