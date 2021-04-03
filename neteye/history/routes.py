c864d086ed38c9b68c416c4c92e56d4c4d882c99
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2021-04-03 21:53:19 +0900

    change datatables source data for rendering speed in all history

--- neteye/history/routes.py
+++ neteye/history/routes.py
@@ -8,10 +8,26 @@ from flask import flash, redirect, render_template, request, session, url_for
 from netaddr import *
 from sqlalchemy.sql import exists
 
-from neteye.apis.history_namespace import (node_transaction_schema,
+from neteye.apis.history_namespace import (arp_entry_transaction_schema,
+                                           arp_entry_transactions_schema,
+                                           arp_entry_version_schema,
+                                           arp_entry_versions_schema,
+                                           cable_transaction_schema,
+                                           cable_transactions_schema,
+                                           cable_version_schema,
+                                           cable_versions_schema,
+                                           interface_transaction_schema,
+                                           interface_transactions_schema,
+                                           interface_version_schema,
+                                           interface_versions_schema,
+                                           node_transaction_schema,
                                            node_transactions_schema,
                                            node_version_schema,
-                                           node_versions_schema)
+                                           node_versions_schema,
+                                           serial_transaction_schema,
+                                           serial_transactions_schema,
+                                           serial_version_schema,
+                                           serial_versions_schema)
 from neteye.blueprints import bp_factory
 from neteye.extensions import connection_pool, db, ntc_template_utils, settings
 
@@ -41,22 +57,27 @@ def node_history():
 @history_bp.route("/interface_history")
 def interface_history():
     interface_history = db.session.query(interface_version, interface_transaction).filter(interface_version.transaction_id == interface_transaction.id).all()
-    return render_template("history/interface_history.html", interface_history=interface_history, OPERATION_TYPE=OPERATION_TYPE)
+    data = [{**(interface_transaction_schema.dump(transaction.Transaction)), **(interface_version_schema.dump(transaction.InterfaceVersion))} for transaction in interface_history]
+    return render_template("history/interface_history.html", data=_pretty_data(data))
 
 
 @history_bp.route("/serial_history")
 def serial_history():
     serial_history = db.session.query(serial_version, serial_transaction).filter(serial_version.transaction_id == serial_transaction.id).all()
-    return render_template("history/serial_history.html", serial_history=serial_history, OPERATION_TYPE=OPERATION_TYPE)
+    data = [{**(serial_transaction_schema.dump(transaction.Transaction)), **(serial_version_schema.dump(transaction.SerialVersion))} for transaction in serial_history]
+    return render_template("history/serial_history.html", data=_pretty_data(data))
 
 
 @history_bp.route("/cable_history")
 def cable_history():
     cable_history = db.session.query(cable_version, cable_transaction).filter(cable_version.transaction_id == cable_transaction.id).all()
-    return render_template("history/cable_history.html", cable_history=cable_history, OPERATION_TYPE=OPERATION_TYPE)
+    data = [{**(cable_transaction_schema.dump(transaction.Transaction)), **(cable_version_schema.dump(transaction.CableVersion))} for transaction in cable_history]
+    print(data)
+    return render_template("history/cable_history.html", data=_pretty_data(data))
 
 
 @history_bp.route("/arp_entry_history")
 def arp_entry_history():
     arp_entry_history = db.session.query(arp_entry_version, arp_entry_transaction).filter(arp_entry_version.transaction_id == arp_entry_transaction.id).all()
-    return render_template("history/arp_entry_history.html", arp_entry_history=arp_entry_history, OPERATION_TYPE=OPERATION_TYPE)
+    data = [{**(arp_entry_transaction_schema.dump(transaction.Transaction)), **(arp_entry_version_schema.dump(transaction.ArpEntryVersion))} for transaction in arp_entry_history]
+    return render_template("history/arp_entry_history.html", data=_pretty_data(data))
