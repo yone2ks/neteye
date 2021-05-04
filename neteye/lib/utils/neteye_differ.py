61a7eb8ec08a3768debb2edbc0aaa4ef53dccbdf
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2021-05-05 00:06:52 +0900

    fixed delta_commit to work fine even when the order in the list does not match.

--- neteye/lib/utils/neteye_differ.py
+++ neteye/lib/utils/neteye_differ.py
@@ -2,35 +2,29 @@ from dictdiffer import diff
 
 from neteye.base.models import Base
 
-
-def dynamic_filter(model: Base, filter_condition: dict):
-    for attribute, value in filter_condition.items():
-        query = model.query
-        for attribute, value in filter_condition.items():
-            query = query.filter(getattr(model, attribute) == value)
-        return query.first()
-
-
 CHANGE = "change"
 REMOVE = "remove"
 ADD = "add"
 
 
-def delta_commit(model: Base, before: list, after: list) -> None:
-    delta = tuple(diff(before, after))
+def delta_commit(model: Base, before_keys: set, before: dict, after_keys: set, after: dict) -> None:
+    delta = tuple(diff(before_keys, after_keys))
     for diff_type, diff_target, diff_content in delta:
-        if diff_type == CHANGE:
-            index, attribute = diff_target
-            instance = dynamic_filter(model, before[index])
-            setattr(instance, attribute, diff_content[1])
-            instance.commit()
-        elif diff_type == REMOVE:
-            for index, filter_condition in diff_content:
-                instance = dynamic_filter(model, filter_condition)
-                instance.delete()
+        if diff_type == REMOVE:
+            for index, keys in diff_content:
+                for key in keys:
+                    instance = model.query.get(before[key]["id"])
+                    instance.delete()
         elif diff_type == ADD:
-            for index, filter_condition in diff_content:
-                instance = model()
-                for attribute, value in filter_condition.items():
-                    setattr(instance, attribute, value)
-                instance.add()
+            for index, keys in diff_content:
+                for key in keys:
+                    instance = model(**after[key])
+                    instance.add()
+    dup = before_keys & after_keys
+    for key in dup:
+        attr_delta = tuple(diff(before[key], after[key], ignore=set(["id"])))
+        for diff_type, diff_target, diff_content in attr_delta:
+            if diff_type == CHANGE:
+                instance = model.query.get(before[key]["id"])
+                setattr(instance, diff_target, diff_content[1])
+                instance.commit()
