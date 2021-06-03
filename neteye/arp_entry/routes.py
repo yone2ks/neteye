335f7b2c2e155b053b376a1de44b74f1e7b30c86
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2021-06-03 13:14:23 +0900

    change serverside datatable for performance in arp_entry/index.html

--- neteye/arp_entry/routes.py
+++ neteye/arp_entry/routes.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from dynaconf import settings
-from flask import flash, redirect, render_template, request, session, url_for
+from flask import (flash, jsonify, redirect, render_template, request, session,
+                   url_for)
 
+from datatables import ColumnDT, DataTables
 from neteye.apis.arp_entry_namespace import (arp_entries_schema,
                                              arp_entry_schema)
 from neteye.blueprints import bp_factory
@@ -16,6 +18,20 @@ arp_entry_bp = bp_factory("arp_entry")
 
 @arp_entry_bp.route("")
 def index():
-    arp_entries = ArpEntry.query.all()
-    data = arp_entries_schema.dump(arp_entries)
-    return render_template("arp_entry/index.html", arp_entries=arp_entries, data=data)
+    return render_template("arp_entry/index.html")
+
+
+@arp_entry_bp.route("/data")
+def data():
+    columns = [
+        ColumnDT(ArpEntry.id),
+        ColumnDT(ArpEntry.ip_address),
+        ColumnDT(ArpEntry.mac_address),
+        ColumnDT(Node.hostname),
+        ColumnDT(Interface.name),
+        ColumnDT(ArpEntry.vendor),
+    ]
+    query = db.session.query().select_from(ArpEntry).join(Node).join(Interface)
+    params = request.args.to_dict()
+    row_table = DataTables(params, query, columns)
+    return jsonify(row_table.output_result())
