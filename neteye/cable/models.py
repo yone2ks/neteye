0812f6ac5936e52aa36578240ec8c3afdf2ee0d3
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2021-07-11 20:51:40 +0900

    description default value is blank

--- neteye/cable/models.py
+++ neteye/cable/models.py
@@ -13,6 +13,6 @@ class Cable(Base):
     src_interface = relationship("Interface", foreign_keys=[src_interface_id])
     dst_interface_id = Column(UUIDType(binary=False), ForeignKey("interfaces.id"))
     dst_interface = relationship("Interface", foreign_keys=[dst_interface_id])
-    description = Column(String)
+    description = Column(String, default="")
     cable_type = Column(String)
     link_speed = Column(String)
