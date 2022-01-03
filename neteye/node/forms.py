e99f1faf7cac98b7517f9f41e4cbd247aeb2dc23
yone2ks <t.yoneda.pub@gmail.com>
2022-01-03 16:11:27 +0900

    add ip address validation and fix validation method Require->InputRequire

--- neteye/node/forms.py
+++ neteye/node/forms.py
@@ -1,21 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, Required
+from wtforms.validators import InputRequired, IPAddress
 
 
 class NodeForm(FlaskForm):
-    hostname = StringField("Hostname:", validators=[Required()])
+    hostname = StringField("Hostname:", validators=[InputRequired()])
     description = StringField("Description:")
-    ip_address = StringField("IP Address:", validators=[Required()])
-    port = IntegerField("Port:", default=22, validators=[Required()])
-    device_type = StringField("Device Type:", validators=[Required()])
+    ip_address = StringField("IP Address:", validators=[InputRequired(), IPAddress()])
+    port = IntegerField("Port:", default=22, validators=[InputRequired()])
+    device_type = StringField("Device Type:", validators=[InputRequired()])
     napalm_driver = StringField("Napalm Driver:")
     scrapli_driver = StringField("Scrapli Driver:")
     model = StringField("Model:")
     os_type = StringField("OS Type:")
     os_version = StringField("OS Version:")
-    username = StringField("Username:", validators=[Required()])
-    password = PasswordField("Password:", validators=[Required()])
-    enable = PasswordField("Enable:", validators=[Required()])
+    username = StringField("Username:", validators=[InputRequired()])
+    password = PasswordField("Password:", validators=[InputRequired()])
+    enable = PasswordField("Enable:", validators=[InputRequired()])
     submit = SubmitField("Submit")
     reset = SubmitField("Reset")
