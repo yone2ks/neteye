2b1182c4b6f58940f5ce10463b104c3daed64fd0
yone2ks <t.yoneda.pub@gmail.com>
2022-01-22 23:02:24 +0900

    fix validation method Require->InputRequire and use macro for rendering error message

--- neteye/serial/forms.py
+++ neteye/serial/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Required
+from wtforms.validators import DataRequired, InputRequired
 
 from neteye.node.models import Node
 
@@ -11,10 +11,10 @@ def get_nodes():
 
 class SerialForm(FlaskForm):
     node_id = QuerySelectField(
-        "Node:", validators=[Required()], query_factory=get_nodes, get_label="hostname"
+        "Node:", validators=[InputRequired()], query_factory=get_nodes, get_label="hostname"
     )
-    serial_number = StringField("Serial Number:", validators=[Required()])
-    product_id = StringField("Product ID:", validators=[Required()])
+    serial_number = StringField("Serial Number:", validators=[InputRequired()])
+    product_id = StringField("Product ID:", validators=[InputRequired()])
     description = StringField("Description:")
     submit = SubmitField("Submit")
     reset = SubmitField("Reset")
