1f517e47e815d8f4bf549cc2a551fdc70da0f4d9
yone2ks <t.yoneda.pub@gmail.com>
2023-09-29 23:30:55 +0900

    add Command History model

--- neteye/history/models.py
+++ neteye/history/models.py
@@ -1,5 +1,10 @@
 from sqlalchemy_continuum import transaction_class, version_class
+from sqlalchemy import (Boolean, Column, DateTime, Float, ForeignKey, Integer,
+                        String, Text)
+from sqlalchemy.orm import backref, relationship
 
+
+from neteye.base.models import Base
 from neteye.arp_entry.models import ArpEntry
 from neteye.cable.models import Cable
 from neteye.interface.models import Interface
@@ -18,3 +23,21 @@ arp_entry_version = version_class(ArpEntry)
 arp_entry_transaction = transaction_class(ArpEntry)
 
 OPERATION_TYPE = {0: "INSERT", 1: "UPDATE", 2: "DELETE"}
+
+
+class CommandHistory(Base):
+    __tablename__ = "command_histories"
+
+    username = Column(String, nullable=False)
+    node_id = Column(String, nullable=False)
+    hostname = Column(String, nullable=False)
+    command = Column(String, nullable=False)
+    result = Column(Text)
+
+    def __init__(self, **kwargs):
+        super(CommandHistory, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return "<CommandHistory id={id} date={date} username={username} node_id={node_id} hostname={hostname} command={command}".format(
+            id=self.id, date=self.created_at, username=self.username, node_id=self.node_id, hostname=self.hostname, command=self.command
+        )
