32908e9ca3778c403093225344e22ed5739877a7
yone2ks <t.yoneda.pub@gmail.com>
2023-10-10 21:01:43 +0900

    add authentication in all management url

--- neteye/management/routes.py
+++ neteye/management/routes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from dynaconf import settings
 from flask import flash, redirect, render_template, request, session, url_for
+from flask_security import auth_required, current_user
 
 from neteye.blueprints import bp_factory
 from neteye.extensions import connection_pool, db
@@ -10,6 +11,7 @@ management_bp = bp_factory("management")
 
 
 @management_bp.route("/connection_pool")
+@auth_required()
 def connection_pool_index():
     return render_template(
         "management/connection_pool_index.html", connection_pool=connection_pool.pool
@@ -17,6 +19,7 @@ def connection_pool_index():
 
 
 @management_bp.route("/connection_pool/<ip_address>_<driver_type>/recreate")
+@auth_required()
 def connection_pool_reconnect(ip_address, driver_type):
     node = Node.query.filter(Node.ip_address == ip_address).first()
     connection_pool.recreate_connection(node, driver_type)
@@ -26,6 +29,7 @@ def connection_pool_reconnect(ip_address, driver_type):
 
 
 @management_bp.route("/connection_pool/<ip_address>_<driver_type>/delete", methods=["POST"])
+@auth_required()
 def connection_pool_delete(ip_address, driver_type):
     node = Node.query.filter(Node.ip_address == ip_address).first()
     connection_pool.delete_connection(node, driver_type)
