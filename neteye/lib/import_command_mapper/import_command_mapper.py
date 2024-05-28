2398061003ea3db3fbcbe7a3eb235d558828e4f7
yone2ks <t.yoneda.pub@gmail.com>
2024-05-28 14:39:24 +0900

    add logging and type, docstring, exception process

--- neteye/lib/import_command_mapper/import_command_mapper.py
+++ neteye/lib/import_command_mapper/import_command_mapper.py
@@ -1,5 +1,8 @@
 import json
 import os
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 class ImportCommandMapper:
@@ -8,9 +11,27 @@ class ImportCommandMapper:
     mapping_dict: dict
     SUFFIX = ".json"
 
-    def __init__(self, device_type):
+    def __init__(self, device_type: str) -> None:
+        """
+        Initialize ImportCommandMapper.
+
+        Args:
+            device_type (str): The device type.
+
+        Raises:
+            FileNotFoundError: If the mapping file is not found.
+
+        Returns:
+            None
+        """
         self.device_type = device_type
-        with open(
-            self.MAPPING_DIR + self.device_type + self.SUFFIX, "r"
-        ) as mapping_json:
+        mapping_file_path = os.path.join(
+            self.MAPPING_DIR, f"{self.device_type}{self.SUFFIX}"
+        )
+        if not os.path.isfile(mapping_file_path):
+            logger.error(f"Import command mapping file '{mapping_file_path}' not found")
+            raise FileNotFoundError(
+                f"Import command mapping file '{mapping_file_path}' not found"
+            )
+        with open(mapping_file_path, "r") as mapping_json:
             self.mapping_dict = json.load(mapping_json)
