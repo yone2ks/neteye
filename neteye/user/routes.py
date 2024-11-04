1141461d4daf86a75ffc958017b6f4131eea6edc
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2024-11-05 00:29:03 +0900

    add the view for user management

--- neteye/user/routes.py
+++ neteye/user/routes.py
@@ -1,10 +1,12 @@
 from logging import getLogger
 
-from flask import flash, redirect, render_template, request, session, url_for
+from datatables import ColumnDT, DataTables
+from flask import flash, jsonify, redirect,render_template, request, session, url_for
 from flask_security import auth_required, roles_required, RegisterForm, hash_password
 
+from neteye.extensions import db
 from neteye.blueprints import bp_factory, root_bp
-from neteye.user.models import user_datastore, admin_role, user_role
+from neteye.user.models import user_datastore, admin_role, user_role, User, Role, RolesUsers
 
 logger = getLogger(__name__)
 
@@ -27,3 +29,40 @@ def register():
         flash('User registered successfully.', 'success')
         return redirect(url_for(".index")) 
     return render_template('user/register.html', register_user_form=form)
+
+    
+@user_bp.route('')
+@roles_required('admin')
+def index():
+    return render_template('user/index.html')
+
+
+@user_bp.route('/data')
+@auth_required()
+def data():
+    columns = [
+        ColumnDT(User.id),
+        ColumnDT(User.email),
+        ColumnDT(User.username),
+        ColumnDT(Role.name),
+        ColumnDT(User.active),
+    ]
+    query = db.session.query().select_from(User).join(RolesUsers).join(Role)
+    params = request.args.to_dict()
+    row_table = DataTables(params, query, columns)
+    return jsonify(row_table.output_result())
+
+
+@user_bp.route('/<id>')
+@auth_required()
+def show(id):
+    user = User.query.get(id)
+    return render_template('user/show.html', user=user)
+
+
+@user_bp.route('/<id>/delete', methods=['POST'])
+@auth_required()
+def delete(id):
+    user = User.query.get(id)
+    user.delete()
+    return redirect(url_for("user.index"))
