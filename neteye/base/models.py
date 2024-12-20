3d777ad137f165965ea833d6a02d2fe0359722cb
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2024-12-20 11:01:59 +0900

    change raising exception because IntegirityError.orig changes exceptions depending on the DB used

--- neteye/base/models.py
+++ neteye/base/models.py
@@ -2,8 +2,7 @@ import uuid
 from datetime import datetime, timezone
 
 from flask_continuum import VersioningMixin
-from sqlalchemy import (Boolean, Column, DateTime, Float, ForeignKey, Integer,
-                        String)
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.sql import func
 from neteye.extensions import db
@@ -12,36 +11,40 @@ from neteye.extensions import db
 def gen_uuid_str():
     return str(uuid.uuid4())
 
+
 class Base(db.Model, VersioningMixin):
     __abstract__ = True
 
     id = Column(String, primary_key=True, default=gen_uuid_str)
     created_at = Column(DateTime, nullable=False, default=func.now())
-    updated_at = Column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
+    updated_at = Column(
+        DateTime, nullable=False, default=func.now(), onupdate=func.now()
+    )
 
     def __repr__(self):
-        return "<id={id} created_at={created_at} updated_at={updated_at}".format(
-            id=self.id, created_at=self.created_at, updated_at=self.updated_at
-        )
+        return f"<{self.__class__.__name__}(id={self.id}, created_at={self.created_at.isoformat()}, updated_at={self.updated_at.isoformat()})>"
 
     def commit(self):
         try:
             db.session.commit()
-        except IntegrityError as sql_integrity_error:
+        except Exception as e:
             db.session.rollback()
-            raise sql_integrity_error.orig
+            raise
 
     def add(self):
         try:
             db.session.add(self)
             self.commit()
-        except IntegrityError as sql_integrity_error:
+        except Exception as e:
             db.session.rollback()
-            raise sql_integrity_error.orig
+            raise
 
     def delete(self):
         db.session.delete(self)
         self.commit()
 
     def rollback(self):
-        db.session.rollback()
+        try:
+            db.session.rollback()
+        except Exception as e:
+            raise
