494bfded1970960d2c95e3221d6cd18194c88b7b
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2024-12-29 00:24:17 +0900

    add error handling for classifing integirity_error

--- neteye/cable/routes.py
+++ neteye/cable/routes.py
@@ -1,8 +1,11 @@
+from logging import getLogger
+
 import pandas as pd
 from dynaconf import settings
 from flask import flash, jsonify, redirect, render_template, request, session, url_for
 from flask_security import auth_required, current_user
 from sqlalchemy.orm import aliased
+from sqlalchemy.exc import IntegrityError
 
 from datatables import ColumnDT, DataTables
 from neteye.apis.cable_namespace import cable_schema, cables_schema
@@ -10,9 +13,12 @@ from neteye.blueprints import bp_factory
 from neteye.extensions import db
 from neteye.interface.models import Interface
 from neteye.node.models import Node
+from neteye.lib.utils.integrity_error_utils import gen_integrity_error_message
 
 from .forms import CableForm
 from .models import Cable
+
+logger = getLogger(__name__)
 
 cable_bp = bp_factory("cable")
 a_interface_table = aliased(Interface)
@@ -86,7 +92,18 @@ def create():
         link_speed=request.form["link_speed"],
         description=request.form["description"],
     )
-    cable.add()
+    try:
+        cable.add()
+    except IntegrityError as e:
+        cable.rollback()
+        logger.warning(f"IntegrityError: {e}")
+        flash(gen_integrity_error_message("Cable", e), "danger")
+        return redirect(url_for("cable.new"))
+    except Exception as e:
+        cable.rollback()
+        logger.error(f"Unexpected Error: {e}")
+        flash("An unexpected error occurred while creating the cable.", "danger")
+        return redirect(url_for("cable.new"))
     return redirect(url_for("cable.index"))
 
 
@@ -102,7 +119,6 @@ def edit(id):
     return render_template(
         "cable/edit.html",
         id=id,
-
         form=form,
         a_node_id=a_interface.node_id,
         b_node_id=b_interface.node_id,
@@ -121,7 +137,18 @@ def update(id):
     cable.b_interface_id = request.form["b_interface"]
     cable.cable_type = request.form["cable_type"]
     cable.link_speed = request.form["link_speed"]
-    cable.commit()
+    try:
+        cable.commit()
+    except IntegrityError as e:
+        cable.rollback()
+        logger.warning(f"IntegrityError: {e}")
+        flash(gen_integrity_error_message("Cable", e), "danger")
+        return redirect(url_for("cable.edit", id=id))
+    except Exception as e:
+        cable.rollback()
+        logger.error(f"Unexpected Error: {e}")
+        flash("An unexpected error occurred while updating the cable.", "danger")
+        return redirect(url_for("cable.edit", id=id))
     return redirect(url_for("cable.index"))
 
 
